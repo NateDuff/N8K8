008152e8b6e04219d10ca84b0b8497862ab0cff5
Nate Duff <nate@duffcloudservices.com>
2024-05-16 13:52:51 -0400

    Creating Worker & Job projects & K8s deployments

--- run.ps1
+++ run.ps1
@@ -3,3 +3,7 @@ kubectl apply -f ./.k8s/api-deployment.yaml
 
 kubectl apply -f ./.k8s/web-service.yaml
 kubectl apply -f ./.k8s/api-service.yaml
+
+kubectl apply -f ./.k8s/worker-deployment.yaml
+
+#kubectl apply -f ./.k8s/job-deployment.yaml
